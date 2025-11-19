54ad11ad6d10c9965a180710a874614e6d9188b0
devopspractice45a3 <devopspractice45a3@gmail.com>
2025-11-19 08:23:56 +0530

    added chanes

--- vars/buildProject.groovy
+++ vars/buildProject.groovy
@@ -1,3 +1,14 @@
-def call(){
- echo "this is build function"
+def call(Map args = [:]){
+    def tool = args.tool ?: "maven"
+    def phase = args.phase ?: "clean install" 
+    def cmd = ""
+    if (tool == 'maven'){
+        cmd = "mvn ${phase}" 
+    }else if (tool == 'gradle'){
+        cmd = "./gradlew ${phase}"
+    }else if (tool == "npm") {
+        cmd = "npm ${phase}"
+    }
+
+bat cmd
 }
